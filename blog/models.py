bee3b84482100592cafa8a78b724dda06e7670f1
jjy0307 <adz7546@gmail.com>
2022-06-16 18:09:54 +0900

    0616 DRF DAY2

---
+++ blog/models.py
@@ -0,0 +1,22 @@
+from tkinter import CASCADE
+from django.db import models
+from user.models import User
+
+# Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=20, unique=True)
+    explain = models.TextField()
+
+    def __str__(self):
+        return self.category_name
+
+
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    category = models.ManyToManyField(Category)
+
+    def __str__(self):
+        return f'{self.title} - {self.author}'
---
+++ user/views.py
@@ -0,0 +1,33 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from django.contrib.auth import login, authenticate, logout
+
+
+# 유저
+class UserView(APIView):
+    permission_classes = [permissions.AllowAny]
+
+    def get(self, request):
+        return Response({'message': 'this is get method!'})
+
+
+# 로그인 로그아웃 기능
+class UserApiView(APIView):
+    # 로그인
+    def post(self, request):
+        user = authenticate(request, **request.data)
+
+        if not user:
+            msg = '아이디 또는 패스워드를 확인해주세요.'
+            return Response({'message': msg})
+
+        login(request, user)
+        msg = '로그인 성공!'
+        return Response({'message': msg}, status=status.HTTP_200_OK)
+
+    # 로그아웃
+    def delete(self, request):
+        logout(request)
+        msg = '로그아웃 성공!'
+        return Response({'message': msg})
