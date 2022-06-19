70712f1275a3ac5206cf424c483f7f6ad6cf7b7e
jjy0307 <adz7546@gmail.com>
2022-06-19 21:58:16 +0900

    0617 DRF DAY3

--- blog/models.py
+++ blog/models.py
@@ -20,3 +20,12 @@ class Article(models.Model):
 
     def __str__(self):
         return f'{self.title} - {self.author}'
+
+
+class Comment(models.Model):
+    comment = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.article.title} comment - {self.author.username}'
